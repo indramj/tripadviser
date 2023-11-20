c4851909c1970406cf1de3a8ebc2b2b57c5558d9
KMJ <holygrail7@naver.com>
2023-11-20 10:51:47 +0900

    Initial commit

---
+++ src/main/java/com/first/tripadviser/TripadviserApplication.java
@@ -0,0 +1,13 @@
+package com.first.tripadviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripadviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripadviserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/tripadviser/TripadviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.tripadviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripadviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
